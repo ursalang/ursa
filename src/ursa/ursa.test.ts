c4784a12ea426ae75c4bf3f227a06f3b9a5e0e54
Reuben Thomas <rrt@sc3d.org>
2023-08-25 21:35:52 +0100

    Ursa: update short tests to same simple format as Ark's

--- src/ursa/ursa.test.ts
+++ src/ursa/ursa.test.ts
@@ -9,37 +9,45 @@ import {
 
 Error.stackTraceLimit = Infinity
 
-test('Comment', (t) => {
-  t.is(valueOf(evalArk(toVal('// Comment'), new EnvironmentVal([]))), null)
-  t.is(valueOf(evalArk(toVal('// Comment\n3'), new EnvironmentVal([]))), 3)
-})
+function testGroup(title: string, tests: [string, any][]) {
+  test(title, (t) => {
+    for (const [source, expected] of tests) {
+      t.deepEqual(valueOf(evalArk(toVal(source), new EnvironmentVal([]))), expected)
+    }
+  })
+}
 
-test('Concrete values', (t) => {
-  t.is(valueOf(evalArk(toVal('4'), new EnvironmentVal([]))), 4)
-  t.is(valueOf(evalArk(toVal(String.raw`"hello \u00e9"`), new EnvironmentVal([]))), 'hello é')
-})
+testGroup('Comment', [
+  ['// Comment', null],
+  ['// Comment\n3', 3],
+])
 
-test('Intrinsics', (t) => {
-  t.is(valueOf(evalArk(toVal('3 + 4'), new EnvironmentVal([]))), 7)
-  t.is(valueOf(evalArk(toVal('(3 + 4) * 5'), new EnvironmentVal([]))), 35)
-  t.is(valueOf(evalArk(toVal('pi'), new EnvironmentVal([]))), Math.PI)
-  t.is(valueOf(evalArk(toVal('3 + 4 == 7'), new EnvironmentVal([]))), true)
-  t.is(valueOf(evalArk(toVal('not 2'), new EnvironmentVal([]))), false)
-})
+testGroup('Concrete values', [
+  ['4', 4],
+  [String.raw`"hello \u00e9"`, 'hello é'],
+])
 
-test('Sequences', (t) => {
-  t.is(valueOf(evalArk(toVal('{ pi }'), new EnvironmentVal([]))), Math.PI)
-  t.is(valueOf(evalArk(toVal('{ pi; 3+4 }'), new EnvironmentVal([]))), 7)
-  t.is(valueOf(evalArk(toVal('{ pi; 3+4; }'), new EnvironmentVal([]))), 7)
-})
+testGroup('Intrinsics', [
+  ['3 + 4', 7],
+  ['(3 + 4) * 5', 35],
+  ['pi', Math.PI],
+  ['3 + 4 == 7', true],
+  ['not 2', false],
+])
 
-test('Conditionals', (t) => {
-  t.is(valueOf(evalArk(toVal('if true {3} else {4}'), new EnvironmentVal([]))), 3)
-  t.is(valueOf(evalArk(toVal('if false {3} else {4}'), new EnvironmentVal([]))), 4)
-  t.is(valueOf(evalArk(toVal('if 3 + 4 == 7 {1} else {0}'), new EnvironmentVal([]))), 1)
-  t.is(valueOf(evalArk(toVal('1 or 2'), new EnvironmentVal([]))), 1)
-  t.is(valueOf(evalArk(toVal('1 and 2'), new EnvironmentVal([]))), 2)
-})
+testGroup('Sequences', [
+  ['{ pi }', Math.PI],
+  ['{ pi; 3+4 }', 7],
+  ['{ pi; 3+4; }', 7],
+])
+
+testGroup('Conditionals', [
+  ['if true {3} else {4}', 3],
+  ['if false {3} else {4}', 4],
+  ['if 3 + 4 == 7 {1} else {0}', 1],
+  ['1 or 2', 1],
+  ['1 and 2', 2],
+])
 
 test('loop and break', (t) => {
   const error = t.throws(() => evalArk(toVal('break'), new EnvironmentVal([])), {instanceOf: BreakException})
@@ -49,26 +57,26 @@ test('loop and break', (t) => {
   t.is(valueOf(evalArk(toVal('loop { break 3 }'), new EnvironmentVal([]))), 3)
 })
 
-test('let', (t) => {
-  t.is(valueOf(evalArk(toVal('let a = 3; a'), new EnvironmentVal([]))), 3)
-  t.is(valueOf(evalArk(toVal('let b = 5; b = 7; b'), new EnvironmentVal([]))), 7)
-})
+testGroup('let', [
+  ['let a = 3; a', 3],
+  ['let b = 5; b = 7; b', 7],
+])
 
-test('fn', (t) => {
-  t.is(valueOf(evalArk(toVal('let f = fn(x) {x + 1}; f(1)'), new EnvironmentVal([]))), 2)
-})
+testGroup('fn', [
+  ['let f = fn(x) {x + 1}; f(1)', 2],
+])
 
-test('Lists', (t) => {
-  t.deepEqual(valueOf(evalArk(toVal('[1, 2, 3]'), new EnvironmentVal([]))), [1, 2, 3])
-  t.is(valueOf(evalArk(toVal('[1, 2, 3].length'), new EnvironmentVal([]))), 3)
-  t.is(valueOf(evalArk(toVal('[1, 2, 3][1]'), new EnvironmentVal([]))), 2)
-})
+testGroup('Lists', [
+  ['[1, 2, 3]', [1, 2, 3]],
+  ['[1, 2, 3].length', 3],
+  ['[1, 2, 3][1]', 2],
+])
 
-test('Objects', (t) => {
-  t.deepEqual(valueOf(evalArk(toVal('{}'), new EnvironmentVal([]))), {})
-})
+testGroup('Objects', [
+  ['{}', {}],
+])
 
-test('Maps', (t) => {
-  t.deepEqual(valueOf(evalArk(toVal('{"a": 1, "b": 2 + 0, 3: 4}'), new EnvironmentVal([]))), new Map<any, any>([['a', 1], ['b', 2], [3, 4]]))
-  t.deepEqual(valueOf(evalArk(toVal('let t = {"a": 1, "b": 2 + 0, 3: 4}; t["b"] = 1; t'), new EnvironmentVal([]))), new Map<any, any>([['a', 1], ['b', 1], [3, 4]]))
-})
+testGroup('Maps', [
+  ['{"a": 1, "b": 2 + 0, 3: 4}', new Map<any, any>([['a', 1], ['b', 2], [3, 4]])],
+  ['let t = {"a": 1, "b": 2 + 0, 3: 4}; t["b"] = 1; t', new Map<any, any>([['a', 1], ['b', 1], [3, 4]])],
+])
