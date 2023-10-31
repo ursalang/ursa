252b08e06046fbf5d3365d78e5a82ae9a89aae68
Reuben Thomas <rrt@sc3d.org>
2023-10-31 21:45:40 +0000

    Ursa: make the REPL usable with `let` (fix #5)
    Parse an `Exp` at a time, and deal with bound variables in `repl()`,
    maintaining a persistent stack frame that accumulates `let` bindings.
    
    This allows the following to work:
    
    ```
    > let a = 3
    3
    > a
    3
    ```

--- src/ursa/cli.ts
+++ src/ursa/cli.ts
@@ -7,11 +7,11 @@ import {ArgumentParser, RawDescriptionHelpFormatter} from 'argparse'
 import assert from 'assert'
 import programVersion from '../version.js'
 import {
-  debug, List, ValRef, Str, globals, ArkState,
+  debug, List, ValRef, Str, globals, ArkState, Undefined,
 } from '../ark/interp.js'
 import {toJs} from '../ark/ffi.js'
 import {serialize} from '../ark/serialize.js'
-import {compile as arkCompile} from '../ark/compiler.js'
+import {Environment, compile as arkCompile} from '../ark/compiler.js'
 import {compile as ursaCompile} from './compiler.js'
 
 if (process.env.DEBUG) {
@@ -49,6 +49,7 @@ your option) any later version. There is no warranty.`,
 interface Args {
   module: string
   eval: string
+  // FIXME: JSON syntax is not compatible with the REPL.
   syntax: string
   compile: boolean
   output: string | undefined
@@ -57,19 +58,14 @@ interface Args {
 }
 const args: Args = parser.parse_args() as Args
 
-function compile(exp: string) {
+function compile(exp: string, env: Environment = new Environment(), startRule?: string) {
   const compiler = args.syntax === 'json' ? arkCompile : ursaCompile
-  const compiled = compiler(exp)
+  const compiled = compiler(exp, env, startRule)
   if (process.env.DEBUG) {
     console.log('Compiled Ark')
     debug(compiled, null)
   }
   return compiled
-}
-
-function evaluate(ark: ArkState, exp: string) {
-  const compiled = compile(exp)
-  return ark.run(compiled)
 }
 
 async function repl() {
@@ -81,10 +77,21 @@ async function repl() {
   })
   rl.prompt()
   const ark = new ArkState()
+  let env = new Environment()
   let val
   for await (const line of rl) {
     try {
-      val = toJs(evaluate(ark, line))
+      const compiled = compile(line, env, 'Exp')
+      // Filter out already-declared bindings
+      for (const id of env.stack[0]) {
+        compiled[1].delete(id)
+      }
+      // Handle new let bindings
+      if (compiled[2] !== undefined && compiled[2].length > 0) {
+        env = env.push(compiled[2])
+        ark.stack = ark.stack.push(Array(compiled[2].length).fill(new ValRef(Undefined)))
+      }
+      val = toJs(ark.run(compiled))
       console.dir(val, {depth: null})
     } catch (error) {
       if (error instanceof Error) {
@@ -149,7 +156,7 @@ async function main() {
       )))
       // Run the program
       if (source !== undefined) {
-        result = evaluate(new ArkState(), source)
+        result = new ArkState().run(compile(source))
       }
       if (source === undefined || args.interactive) {
         result = await repl()
