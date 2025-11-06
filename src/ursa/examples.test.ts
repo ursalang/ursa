d427f8db2302244154b1019495b88c6963a28de0
Daniel Shindler <danielshindler@yahoo.co.uk>
2025-11-06 22:32:14 +0000

    Add new Rosetta Code example Loops/For

--- src/ursa/examples.test.ts
+++ src/ursa/examples.test.ts
@@ -71,6 +71,7 @@ import {
   // This program produces non-deterministic output
   // ['Loops/Break', 'rosettacode/Loops-Break'],
   ['Loops/Continue', 'rosettacode/Loops-Continue'],
+  ['Loops/For', 'rosettacode/Loops-For'],
   ['Loops/While', 'rosettacode/Loops-While'],
   ['Man or boy test', 'rosettacode/Man or boy test'],
 ].map(([title, file]) => ursaTest(title, file))
